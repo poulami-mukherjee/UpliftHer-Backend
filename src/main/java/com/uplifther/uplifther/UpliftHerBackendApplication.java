6ed48d6de3d1340982de70e3c54cf1b33fa67bbd
Poulami <mukherjeepoulami0511@gmail.com>
2023-10-17 18:49:42 +0530

    [Add] Add Initial Configuration for Spring Boot 3.1.4, Java 17, Lombok, Spring Web, Spring Security and Spring Data JPA

---
+++ src/main/java/com/uplifther/uplifther/UpliftHerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uplifther.uplifther;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpliftHerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpliftHerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uplifther/uplifther/UpliftHerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplifther.uplifther;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpliftHerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
