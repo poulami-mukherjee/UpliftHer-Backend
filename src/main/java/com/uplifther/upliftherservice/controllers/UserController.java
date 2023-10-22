7018cdc25242b0c24ef00b3eb48eeb667c69f353
Aseem Sharan <sharan.aseem@gmail.com>
2023-10-23 00:25:03 +0530

    Added insights API template

---
+++ src/main/java/com/uplifther/upliftherservice/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.uplifther.upliftherservice.controllers;
+
+public class UserController {
+}
---
+++ src/main/java/com/uplifther/upliftherservice/model/Insight.java
@@ -0,0 +1,4 @@
+package com.uplifther.upliftherservice.model;
+
+public record Insight(String name) {
+}
